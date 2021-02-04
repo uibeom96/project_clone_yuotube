1c6ccbf4eddcbf1b37a8c2c618c09ab7f410b2bc
uibeom96 <dmlqja7570@daum.net>
2021-02-05 03:28:27 +0900

    youtube main / detail 2개 html 구현완료

--- youtube/urls.py
+++ youtube/urls.py
@@ -4,5 +4,6 @@ from youtube import views
 app_name = "youtube"
 
 urlpatterns = [
-    path("", views.index, name="main")
+    path("", views.index, name="index"), 
+    path("detail/", views.detail, name="detail"),
 ]
--- youtube/views.py
+++ youtube/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 
-
 def index(request):
-    return render(request, "youtube/main.html")
+    return render(request, "youtube/index.html")
+
+
+def detail(request):
+    return render(request, "youtube/detail.html")
